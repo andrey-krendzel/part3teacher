6239a4374a26b7a563a048f15120e91e9fd5cf7a
unknown <andreykrendzel@hotmail.com>
2021-06-16 06:49:04 +0100

    Updators

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -56,7 +58,7 @@ app.get('/api/notes/:id', (request, response, next) => {
   .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (body.content === undefined) {
@@ -72,6 +74,8 @@ app.post('/api/notes', (request, response) => {
   note.save().then(savedNote => {
     response.json(savedNote)
   })
+
+  .catch(error => next(error))
 })
 
 
