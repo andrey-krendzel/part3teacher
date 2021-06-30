c855a581b2e20731d4ff8a1a184c652bd869e72a
unknown <andreykrendzel@hotmail.com>
2021-06-30 12:02:53 +0100

    nobody said it was eazy

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
+const loginRouter = require('./controllers/login')
+
 const mongoose = require('mongoose')
 
 logger.info('connecting to', config.MONGODB_URI)
@@ -26,6 +28,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,12 @@
 const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
+const Note = require('../models/note')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User
+  .find({}).populate('notes', { content: 1, date: 1 })
+
   response.json(users)
 })
 
